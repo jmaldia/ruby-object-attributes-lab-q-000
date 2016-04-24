1b772ab46689015b97289cbe9f3978ac98fa72c9
jmaldia <jmaldia@activecampaign.com>
2016-04-23 20:14:30 -0500

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,29 @@
+# define dog class
+class Dog
+
+# setters and getters for name
+  def initialize
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+# setters and getters for breed
+  def initialize(breed)
+    @breed
+  end
+
+  def breed
+    @breed
+  end
+
+  def breed=(new_breed)
+    @breed = new_breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,30 @@
+# define dog class
+class Person
+
+# setters and getters for name
+  def initialize(name)
+    @name = name
+  end
+
+  def name
+    @name
+  end
+
+  def name=(new_name)
+    @name = new_name
+  end
+
+# setters and getters for job
+  def initialize(job)
+    @job = job
+  end
+
+  def job
+    @job
+  end
+
+  def job=(new_job)
+    @job = new_job
+  end
+
+end
